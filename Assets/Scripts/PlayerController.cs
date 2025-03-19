3a45eeefb240f64e1a7d7eb5b41921c52aecb4d5
XavierPiboule <piboule.xavier@gmail.com>
2025-03-19 15:53:24 +0100

    Polish & bool Gounded
    hitbox perso réhadapter
    _isGrounded défini si on peut sauter
    test des limites de pastPos (éviter de mettre plus de 2min de sauvegarde)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ public class PlayerController : MonoBehaviour
     private Collider2D _collider2D;
     private SpriteRenderer _spriteRend;
     private Transform _transform;
+    private Collider2D _monCollider;
 
     [SerializeField]private float moveSpeed; //private mais visible dans unity
     [SerializeField]private float jumpPower;
     [SerializeField]private float limiteJump;
+    [SerializeField]private float longueurCheckJump = 1.1f;
 
-    private bool _isGrounded = true;
+    private bool _isGrounded;
+
+    private RaycastHit2D hit;
 
     void Awake()
     {
@@ -19,6 +23,7 @@ public class PlayerController : MonoBehaviour
         TryGetComponent(out _rgbd2D);
         TryGetComponent(out _collider2D);
         TryGetComponent(out _spriteRend);
+        TryGetComponent(out _monCollider);
     }
 
     void Update()
@@ -27,6 +32,7 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
             Jump();
+        IsGrounded();
     }
 
     void Move()
@@ -44,11 +50,28 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
         _rgbd2D.linearVelocityY = jumpPower;
-        Debug.Log(_rgbd2D.linearVelocity);
+        //Debug.Log(_rgbd2D.linearVelocity);
     }
 
     void IsGrounded()
     {
+        // On fait un raycast qui s'adatpe automatiquement à la taille et au positionnement de votre collider, et qui vise vers le bas
+        hit = Physics2D.Raycast(transform.position, Vector2.down, (_monCollider.bounds.extents.y + Mathf.Abs(_monCollider.offset.y) * transform.localScale.y) * longueurCheckJump);
 
+        // Ca c'est juste une ligne pour dessiner le raycast dans l'éditor, histoire de pouvoir l'ajuster avec la variable "longueurCheckJump"
+        Debug.DrawRay(transform.position, Vector2.down * (_monCollider.bounds.extents.y + Mathf.Abs(_monCollider.offset.y) * transform.localScale.y) * longueurCheckJump, Color.red);
+        // Si le raycast touche ET que c'est pas un trigger, c'est qu'il y a un sol sous nos pied, donc on peut sauter, sinon c'est qu'on est déjà en l'air et donc on peut pas sauter
+        if (hit && !hit.collider.isTrigger)
+        {
+            _isGrounded = true;
+            //animatotor.SetBool("jump", false);
+
+            //Debug.Log(hit);
+        }
+        else
+        {
+            _isGrounded = false;
+            //animatotor.SetBool("jump", true);
+        }
     }
 }
